62d0afd688cb76277e83035609ea1c188cb4e394
Julia Valkama <juliavalkama2012@gmail.com>
2025-03-13 15:49:46 +0200

    3.7.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
 let persons = [
@@ -30,6 +31,7 @@ let persons = [
 ]
 
 app.use(express.json())
+app.use(morgan('tiny'))
 
 app.get('/api/persons', (request, response) => {
   response.json(persons)
